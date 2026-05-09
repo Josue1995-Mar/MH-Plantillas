1d9c98b1b3c7f5eaf57e52edd4c5e4d0d4085f55
Ronaldo Hernandez <ronaldojosueguerrerohernandez@gmail.com>
2026-05-09 08:35:52 -0600

    fix cors render production

--- src/main/java/com/app/controller/ZipController.java
+++ src/main/java/com/app/controller/ZipController.java
@@ -7,6 +7,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/zip")
+@CrossOrigin(origins = "*")
 public class ZipController {
 
     private final ZipService zipService;
